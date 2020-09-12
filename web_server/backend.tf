2c1550a45fc7ec9fd67a583dc32eba0284c02575
mcalik77 <mcalikdevop@gmail.com>
2020-09-12 12:03:29 -0700

    fixed

--- web_server/backend.tf
+++ web_server/backend.tf
@@ -1,7 +1,7 @@
- terraform {
- 	backend "s3" {
- 		bucket = "mcalikterraform"
- 		key    = "terraform/terraform.tfstate"
- 		region = "us-west-1"
- 	}
- }
+ #terraform {
+ #	backend "s3" {
+ #		bucket = "mcalikterraform"
+ #		key    = "terraform/terraform.tfstate"
+ #		region = "us-west-1"
+ #	}
+ #}
